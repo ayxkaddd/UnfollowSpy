708d2c31d58447ef59f33583bba1889daa8f6bfd
ayxkaddd <zxcvbqwert390@gmail.com>
2023-01-21 14:32:19 +0200

    add config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+bot_token = "telegram bot token"
+ig_username = "username of fake ig account"
+ig_password = "password of fake ig account"
+ig_account_id = 11111111 # ig id of account you want to track (must be int) 
+admins_id = [111111111111] # list of yours telegram id (must be int)
